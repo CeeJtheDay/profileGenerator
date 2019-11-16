a3231b81ee79710baeda73119c1fb21aeeb78697
CJ Conrad <carlj.conrad@CJs-MacBook-Pro.local>
2019-11-15 18:08:37 -0800

    code finished, testing

--- index.js
+++ index.js
@@ -1,32 +1,70 @@
 //const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
+const generateHTML = require("./generateHTML.js");
+const gPS = require("github-personal-stars");
+const pdf = require('html-pdf');
 
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "userName",
-      message: "What is your GitHub username?"
-    },
-    {
-      type: "list",
-      message: "What color would you like to use?",
-      name: "colorChoice",
-      choices: [
-        "green",
-        "blue",
-        "pink",
-        "red"
-      ]
-    }
-])
-.then(function(res) {
-        console.log("------------\n");
-        console.log(res.userName);
-        console.log("------------\n");
-        console.log(res.colorChoice);
+async function makeProfile() {
+  try {
+    const answers = inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "userName",
+          message: "What is your GitHub username?"
+        },
+        {
+          type: "list",
+          message: "What color would you like to use?",
+          name: "colorChoice",
+          choices: [
+            "green",
+            "blue",
+            "pink",
+            "red"
+          ]
+        }
+      ])
+      .then(function (data) {
+        console.log("------Git Hub User Name------\n");
+        console.log(data.userName);
+        console.log("------Use Color Choice------\n");
+        console.log(data.colorChoice);
+        return data;
+      });
 
+      const bioObj = await axios
+      .get(`https://api.github.com/users/${answers.username}`)
+      .then(res => {
+        console.log("------Bio Object======");
+        console.log(res);
+        return res;
+      });
 
-    });
+      const starCount = await gPS(answers.userName, (err, total) => {
+        console.log("------Total User Stars------");
+        console.log(err || total);
+        return total;
+      });
+
+      fs.writeFile("index.html", generateHTML(answers, bioObj, starCount), err => {
+        if (err) {
+          throw err;
+        } 
+        console.log("index.html Written successfully!");
+      });
+
+      const html = await fs.readFileSync('./index.html', 'utf8');
+      const options = { format: 'Letter' };
+
+      pdf.create(html, options).toFile('./resumePDF', function(err, res) {
+        if (err) return console.log(err);
+        console.log{"HTML successfully converted!"};
+        console.log("------converted file------");
+        console.log(res);
+        ret
+      });
+
+});
